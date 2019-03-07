220fbb7c6eb3290eab28695e8535b5ddd04fd00f
Shrivara <shrivara.jois@gmail.com>
2019-03-07 22:52:29 +0530

    Moves and splits

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,2 @@
 FROM nginx:1.15.9-alpine
-COPY . /usr/share/nginx/html
+COPY ./html /usr/share/nginx/html
---
+++ bin/dev.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+set -eo pipefail
+BASE_DIR=$(dirname "$0")
+
+cd "${BASE_DIR}/.."
+docker build . -t shrivara/shrivara-dev:dev
+docker rm shrivara-dev || true
+docker run -p 80:80 -d --rm --name shrivara-dev -v $(pwd)/html:/usr/share/nginx/html shrivara/shrivara-dev:dev
