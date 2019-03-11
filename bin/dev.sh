fc29400456b82ed23c56bb111fca0f19ad533cf3
Shrivara <shrivara.jois@gmail.com>
2019-03-11 13:20:35 +0530

    Stop dev container before attempting to delete [skip ci]

--- bin/dev.sh
+++ bin/dev.sh
@@ -4,5 +4,6 @@ BASE_DIR=$(dirname "$0")
 
 cd "${BASE_DIR}/.."
 docker build . -t shrivara/shrivara-dev:dev
+docker stop shrivara-dev || true
 docker rm shrivara-dev || true
 docker run -p 80:80 -d --rm --name shrivara-dev -v $(pwd)/html:/usr/share/nginx/html shrivara/shrivara-dev:dev
